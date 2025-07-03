47e2c2f4660b667a6024014a42132f12766c1d84
Ankitsgit <ankit77us@gmail.com>
2025-07-03 13:00:11 +0530

    fetch of api data is done

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -6,13 +6,35 @@ import cards_data from '../../assets/cards/Cards_data'
 
 function TitleCards({title, category}) {
 
+    const [apiData ,setApiData] =useState([]);
+
     const cardsRef = useRef();
+
+    const options = {
+      method: 'GET',
+      headers: {
+
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1Y2Y5ZWI3NzBjZTZjOGZiNWU0ZDE1YmMyNWVkNTE4NyIsIm5iZiI6MTc1MTUyNDg0My41MTYsInN1YiI6IjY4NjYyNWViMjZhMzE3OTJhNDljMmUwNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.WI0kTlzH5USi4IiBkeQDCQ3otPV7kujIDOIRm2t8Y5A'
+        }
+      };
+
+
     const handelWheel = (event) => {
     event.preventDefault();
     cardsRef.current.scrollLeft += event.deltaY;
   };
 
   useEffect(() => {
+         fetch(`https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`, options)
+          .then(res => res.json())
+          // .then(res => console.log(res))
+          .then(res => setApiData(res.results))
+
+
+          .catch(err => console.error(err));
+
+
     cardsRef.current.addEventListener("wheel", handelWheel);
   }, []);
 
@@ -22,10 +44,10 @@ function TitleCards({title, category}) {
       <h2>{title?title :"Popular On Fireflix" }</h2>
       <div className="card-list" ref={cardsRef}>
         {/* //mapping cards_data */}
-        {cards_data.map((card ,index)=>{
+        {apiData.map((card ,index)=>{
             return <div className="card"  key={index}>
-                <img src={card.image}alt="" />
-                <p>{card.name}</p>
+                <img src={`https://image.tmdb.org/t/p/original`+card.backdrop_path}alt="" />
+                <p>{card.original_title}</p>
 
             </div>
 
--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,12 +1,2 @@
-import React from 'react'
-import './Player.css'
-function Player() {
-  return (
-    <div className='player'>
-      
-    </div>
-  )
-}
 
-export default Player
  
